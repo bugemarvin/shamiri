dda98d8a012f939e8dc2e2897b5dadda3150e0c0
Marvin Kurland <bugemarvin@outlook.com>
2024-07-10 16:56:11 +0300

    final endpoint to register a new user to the database with hashed password

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,7 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from werkzeug.security import generate_password_hash
+from models import db, User
 
 auth_bp = Blueprint('auth', __name__)
 
+
 @auth_bp.route('/register', methods=['POST'])
 def register():
-  pass
+    '''
+    Register new user endpoint
+
+    Args:
+      None
+
+    Request:
+      JSON object with username and password
+
+    Action:
+      Create a new user and save it to the database with hashed password
+
+    Returns:
+      str: JSON response
+    '''
+    data = request.get_json()
+    hashed_password = generate_password_hash(
+        data['password'], method='sha256')
+    new_user = User(username=data['username'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'Registered successfully'}), 201
