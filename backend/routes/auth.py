de137238df1d137defaee18360e423f2d60ee22b
Marvin Kurland <bugemarvin@outlook.com>
2024-07-10 17:03:16 +0300

    final endpoint to login a user and generate an access token and return it in a JSON response

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from werkzeug.security import generate_password_hash
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
 
 auth_bp = Blueprint('auth', __name__)
@@ -29,7 +30,29 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'Registered successfully'}), 201
-  
+
+
 @auth_bp.route('/login', methods=['POST'])
 def login():
-  pass
+    '''
+    Login user endpoint
+
+    Args:
+      None
+
+    Request:
+      JSON object with username and password
+
+    Action:
+      Check if the user exists and the password is correct
+      Generate an access token and return it in a JSON response
+
+    Returns:
+      str: JSON response
+    '''
+    data = request.get_json()
+    user = User.query.filter_by(username=data['username']).first()
+    if not user or not check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Invalid credentials'}), 401
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'access_token': access_token}), 200
